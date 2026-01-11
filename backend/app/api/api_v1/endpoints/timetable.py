934b9e1b4c8a064a050cd98d98c8fa53f791af23
Nadjoua2004 <nadjoua.sahnoune@gmail.com>
2026-01-12 00:03:15 +0100

    fixesss

--- backend/app/api/api_v1/endpoints/timetable.py
+++ backend/app/api/api_v1/endpoints/timetable.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, List, Optional
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
